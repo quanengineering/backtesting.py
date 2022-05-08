78a83e9d8be26e1afba80f9a31c98fbe5c44fda7
Quan <toolowner@hotmail.com>
2022-05-08 16:47:19 +0700

    Add histogram indicator plot style

--- backtesting/backtesting.py
+++ backtesting/backtesting.py
@@ -77,7 +77,7 @@ class Strategy(metaclass=ABCMeta):
 
     def I(self,  # noqa: E741, E743
           func: Callable, *args,
-          name=None, plot=True, overlay=None, color=None, scatter=False,
+          name=None, plot=True, overlay=None, color=None, scatter=False, histogram=False, legends=None, histogramms=None,
           **kwargs) -> np.ndarray:
         """
         Declare indicator. An indicator is just an array of values,
@@ -107,6 +107,10 @@ class Strategy(metaclass=ABCMeta):
         If `scatter` is `True`, the plotted indicator marker will be a
         circle instead of a connected line segment (default).
 
+        If `histogram` is `True`, the indicator values will be plotted
+        as a histogram instead of line or circle. When `histogram` is
+        `True`, 'scatter' value will be ignored even if it's set.
+
         Additional `*args` and `**kwargs` are passed to `func` and can
         be used for parameters.
 
@@ -114,6 +118,10 @@ class Strategy(metaclass=ABCMeta):
 
             def init():
                 self.sma = self.I(ta.SMA, self.data.Close, self.n_sma)
+
+         `legends` can be list or array of string values to represent
+        legends on your indicator chart. By default it's set to None,
+        and `name` is used as legends.
         """
         if name is None:
             params = ','.join(filter(None, map(_as_str, chain(args, kwargs.values()))))
@@ -153,7 +161,7 @@ class Strategy(metaclass=ABCMeta):
                 overlay = ((x < 1.4) & (x > .6)).mean() > .6
 
         value = _Indicator(value, name=name, plot=plot, overlay=overlay,
-                           color=color, scatter=scatter,
+                           color=color, scatter=scatter, legends=legends, histogram=histogram, histogramms=histogramms,
                            # _Indicator.s Series accessor uses this:
                            index=self.data.index)
         self._indicators.append(value)
